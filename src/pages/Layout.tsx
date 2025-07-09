928fe71f2e86286c77aae51a726fa60d6e6f36f3
Sven <sven.perkmann@gmail.com>
2025-07-09 14:31:00 +0200

    minor

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -3,10 +3,10 @@ import { Topbar } from "../components/Topbar";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen w-screen overflow-hidden">
       <Sidebar className="shrink-0" />
 
-      <div className="flex flex-1 flex-col">
+      <div className="flex flex-1 flex-col overflow-hidden">
         <Topbar />
 
         <main className="flex-1 overflow-auto bg-base-100">{children}</main>
