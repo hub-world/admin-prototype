5a59dfd2148da0248efa96a956cfcc132b58e5f5
Sven <sven.perkmann@gmail.com>
2025-07-09 13:59:05 +0200

    sidebar links

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,50 @@
 import classNames from "classnames";
-import { Building2Icon, HomeIcon, SettingsIcon, UsersIcon } from "lucide-react";
-import { Link, useLocation } from "react-router";
+import {
+  Building2Icon,
+  CalendarIcon,
+  ChartNoAxesCombinedIcon,
+  CheckSquareIcon,
+  CircleDotIcon,
+  ClipboardListIcon,
+  ContactRoundIcon,
+  DoorOpenIcon,
+  EuroIcon,
+  HomeIcon,
+  MessageSquareIcon,
+  ShieldIcon,
+  TagIcon,
+  UsersIcon,
+} from "lucide-react";
+import { NavLink } from "react-router";
 
 const menuItems = [
   { icon: HomeIcon, label: "Home", path: "/" },
-  { icon: UsersIcon, label: "Users", path: "/users" },
+  { icon: MessageSquareIcon, label: "Messages", path: "/messages" },
+  { icon: CalendarIcon, label: "Reservations", path: "/reservations" },
+  { icon: ContactRoundIcon, label: "Customers", path: "/customers" },
+
+  {
+    icon: DoorOpenIcon,
+    label: "Apartments",
+    path: "/apartments",
+    divider: true,
+  },
   { icon: Building2Icon, label: "Properties", path: "/properties" },
-  { icon: SettingsIcon, label: "Settings", path: "/settings" },
+  { icon: TagIcon, label: "Pricing", path: "/pricing" },
+
+  { icon: CircleDotIcon, label: "Issues", path: "/issues", divider: true },
+  { icon: CheckSquareIcon, label: "Tasks", path: "/tasks" },
+  { icon: ClipboardListIcon, label: "Housekeeping", path: "/housekeeping" },
+  { icon: ShieldIcon, label: "Security", path: "/security" },
+
+  {
+    icon: ChartNoAxesCombinedIcon,
+    label: "Dashboard",
+    path: "/dashboard",
+    divider: true,
+  },
+  { icon: EuroIcon, label: "Finance", path: "/finance" },
+  { icon: UsersIcon, label: "Admins", path: "/admins" },
 ];
 
 type SidebarProps = {
@@ -14,14 +52,12 @@ type SidebarProps = {
 };
 
 export function Sidebar({ className }: SidebarProps) {
-  const location = useLocation();
-
   return (
     <div
       className={classNames("flex h-full w-48 flex-col bg-base-200", className)}
     >
       {/* Logo/Title */}
-      <div className="m-6 mb-4">
+      <div className="mx-6 my-4">
         <h1 className="text-xl font-bold text-base-content">Urban Hub.</h1>
       </div>
 
@@ -29,17 +65,28 @@ export function Sidebar({ className }: SidebarProps) {
       <nav className="flex-1">
         <ul className="menu w-full">
           {menuItems.map((item) => (
-            <li key={item.path}>
-              <Link
-                to={item.path}
-                className={classNames("flex items-center gap-3", {
-                  active: location.pathname === item.path,
-                })}
-              >
-                <item.icon className="h-5 w-5" />
-                {item.label}
-              </Link>
-            </li>
+            <>
+              {item.divider && (
+                <hr
+                  key={`divider-${item.label}`}
+                  className="my-2 border-base-300"
+                />
+              )}
+              <li key={item.label}>
+                <NavLink
+                  to={item.path ?? "/404"}
+                  className={({ isActive }) =>
+                    classNames(
+                      "flex items-center gap-3",
+                      isActive && "menu-focus",
+                    )
+                  }
+                >
+                  {item.icon && <item.icon className="h-5 w-5" />}
+                  {item.label}
+                </NavLink>
+              </li>
+            </>
           ))}
         </ul>
       </nav>
